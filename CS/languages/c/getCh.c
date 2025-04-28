ce8d21ca6674d2b2ff09d3612761cb5aad86af01
alex <alex@DESKTOP-8TKBO3V.attlocal.net>
2025-04-27 22:31:44 -0500

    apr27

--- CS/languages/c/getCh.c
+++ CS/languages/c/getCh.c
@@ -1,9 +1,6 @@
 /*
+	need to make a getCh() function similar to getchar() but this function does not echo and is not canonical
 
-	need to make a getCh() function that does not echo and is not canonical
+	a few problems...this program is supposed to use conio.h (which i think is windows), and the this line while((ch = getCh()) != '\r') the carrage return? i dont know
 
-	termios.h: terminal I/O control (POISX)
-	ncurses.h: terminal-independent screen handling
-	unistd.h: POSIX operating system API
-	fcntl.h: file control operations
 */
