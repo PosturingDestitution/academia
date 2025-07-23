d30a7228b3086cbd91cd33246bd1dd3285eac218
PosturingDestitution <aw6572214@gmail.com>
2025-07-22 21:31:23 -0500

    july22

--- computerScience/cProgramming/getCh.c
+++
@@ -1,6 +0,0 @@
-/*
-	need to make a getCh() function similar to getchar() but this function does not echo and is not canonical
-
-	a few problems...this program is supposed to use conio.h (which i think is windows), and the this line while((ch = getCh()) != '\r') the carrage return? i dont know
-
-*/
---
+++ computerScience/programming/python/new.py
@@ -0,0 +1,11 @@
+"""
+this program is going to ask the user for some numbers. those numbers are going into an array in the order that the user gave them. then the user will be asked "what sorting algorithm would you like to use to sort the array" after this the program will run the corresponding algorithm, and then the array will be displayed to the user along with the amount of time each algorithm took.
+"""
+import numpy as np
+
+
+print("please enter some numbers. the numbers should be positive and between (). after each number press enter.")
+
+a = np.uint64(0)
+print(a)
+print(type(a))
