fa2fe1c051e4c1f6eaeae72413bad76a3839f3bb
PosturingDestituion <aw6572214@gmail.com>
2025-11-20 07:32:08 -0600

    2025-11-20

---
+++ computerScience/programming/new.c
@@ -0,0 +1,15 @@
+/*
+the goal if this program is a quick review: macros and define derectives?, the ternary operator, pointers, enums, structures
+*/
+
+#include <stdio.h>
+
+int number = 7;
+int *iptr = &number;
+
+
+int main(){
+	printf("%d", *iptr);
+	printf("\n");
+	return 0;
+}
